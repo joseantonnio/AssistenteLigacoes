65d9f1f80091de24a104369ce249df1c9e999c2f
José Antonio <jasj_brasil@hotmail.com>
2016-10-06 21:30:58 -0300

    Adicionado projeto CLI

---
+++ AssistenteLigacoesCLI/AssistenteLigacoes/Principal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace AssistenteLigacoes
+{
+
+    class Principal
+    {
+        public static void Main(string[] args)
+        {
+
+            var json = new WebClient().DownloadString("https://raw.githubusercontent.com/joseantonnio/AssistenteLigacoes/master/exemploJson.json");
+            var saida = JsonConvert.DeserializeObject<List<Ramal>>(json);
+
+            foreach (Ramal s in saida)
+            {
+                Console.WriteLine(s.ToString());
+            }
+
+            Console.Read();
+
+        }
+
+   }
+
+}
---
+++ AssistenteLigacoesCLI/AssistenteLigacoes/Ramal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistenteLigacoes
+{
+    class Ramal : Telefone
+    {
+        private int sufixo;
+        private string dataInicio;
+        private string dataFim;
+
+        public Ramal (int sufixo, string dataInicio, string dataFim, int ddd, int prefixo) : base(ddd, prefixo)
+        {
+            this.sufixo = sufixo;
+            this.dataInicio = dataInicio;
+            this.dataFim = dataFim;
+    
+        }
+
+
+        public int Sufixo
+        {
+            get { return sufixo; }
+            set { sufixo = value; }
+        }
+
+        public string DataInicio
+        {
+            get { return dataInicio; }
+            set { dataInicio = value; }
+        }
+
+        public string DataFim
+        {
+            get { return dataFim; }
+            set { dataFim = value; }
+        }
+
+
+
+        //Método Autentica Usuario
+        public void AutenticaUsuario()
+        {
+
+        }
+
+        //Método Desliga Usuario
+        public void DesligaUsuario()
+        {
+
+        }
+
+        //Método Busca Ligacoes
+        public void BuscaLigacoes()
+        {
+
+        }
+
+        //Método Busca Origem 
+        public void BuscaOrigem()
+        {
+
+        }
+
+
+        //Método Busca Destino
+        public void buscaDestino()
+        {
+
+        }
+
+
+        public override string ToString()
+        {
+            return "(" + Ddd + ")" + Prefixo + "-" + Sufixo + " Data Inicio: " + DataInicio+ "Data Final : " + DataFim ;
+        }
+    }
+}
