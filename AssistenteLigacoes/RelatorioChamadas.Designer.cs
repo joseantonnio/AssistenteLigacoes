70f1b1740961a65d09a361d3bdabba4ab3a37e5b
José Antonio <jasj_brasil@hotmail.com>
2016-10-09 13:59:53 -0300

    Verificação de campos e salvar como xls

---
+++ AssistenteLigacoes/RelatorioChamadas.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace AssistenteLigacoes
+{
+    partial class RelatorioChamadas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RelatorioChamadas));
+            this.conteudorelatorio = new System.Windows.Forms.DataGridView();
+            this.origem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.destino = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tempo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.operadora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cidadeorigem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cidadedestino = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelramal = new System.Windows.Forms.Label();
+            this.ramalconsulta = new System.Windows.Forms.MaskedTextBox();
+            this.labelchamadas = new System.Windows.Forms.Label();
+            this.tipochamada = new System.Windows.Forms.ComboBox();
+            this.dataInicial = new System.Windows.Forms.MaskedTextBox();
+            this.labelde = new System.Windows.Forms.Label();
+            this.dataFinal = new System.Windows.Forms.MaskedTextBox();
+            this.labelate = new System.Windows.Forms.Label();
+            this.calendariode = new System.Windows.Forms.MonthCalendar();
+            this.calendarioate = new System.Windows.Forms.MonthCalendar();
+            this.relatoriobuscar = new System.Windows.Forms.Button();
+            this.salvarelatorio = new System.Windows.Forms.Button();
+            this.imprimerelatorio = new System.Windows.Forms.Button();
+            this.printrelatorio = new System.Drawing.Printing.PrintDocument();
+            this.previewrelatorio = new System.Windows.Forms.PrintPreviewDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.conteudorelatorio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // conteudorelatorio
+            // 
+            this.conteudorelatorio.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.conteudorelatorio.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.conteudorelatorio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.conteudorelatorio.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.origem,
+            this.destino,
+            this.tempo,
+            this.operadora,
+            this.cidadeorigem,
+            this.cidadedestino});
+            this.conteudorelatorio.Location = new System.Drawing.Point(20, 110);
+            this.conteudorelatorio.Margin = new System.Windows.Forms.Padding(3, 50, 3, 3);
+            this.conteudorelatorio.Name = "conteudorelatorio";
+            this.conteudorelatorio.Size = new System.Drawing.Size(1014, 398);
+            this.conteudorelatorio.TabIndex = 0;
+            // 
+            // origem
+            // 
+            this.origem.HeaderText = "Origem";
+            this.origem.Name = "origem";
+            this.origem.Width = 120;
+            // 
+            // destino
+            // 
+            this.destino.HeaderText = "Destino";
+            this.destino.Name = "destino";
+            this.destino.Width = 120;
+            // 
+            // tempo
+            // 
+            this.tempo.HeaderText = "Duração";
+            this.tempo.Name = "tempo";
+            this.tempo.Width = 150;
+            // 
+            // operadora
+            // 
+            this.operadora.HeaderText = "Operadora";
+            this.operadora.Name = "operadora";
+            this.operadora.Width = 200;
+            // 
+            // cidadeorigem
+            // 
+            this.cidadeorigem.HeaderText = "Cidade de Origem";
+            this.cidadeorigem.Name = "cidadeorigem";
+            this.cidadeorigem.Width = 250;
+            // 
+            // cidadedestino
+            // 
+            this.cidadedestino.HeaderText = "Cidade de Destino";
+            this.cidadedestino.Name = "cidadedestino";
+            this.cidadedestino.Width = 250;
+            // 
+            // labelramal
+            // 
+            this.labelramal.AutoSize = true;
+            this.labelramal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelramal.Location = new System.Drawing.Point(22, 66);
+            this.labelramal.Name = "labelramal";
+            this.labelramal.Size = new System.Drawing.Size(52, 17);
+            this.labelramal.TabIndex = 1;
+            this.labelramal.Text = "Ramal:";
+            // 
+            // ramalconsulta
+            // 
+            this.ramalconsulta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ramalconsulta.Location = new System.Drawing.Point(80, 63);
+            this.ramalconsulta.Mask = "0000";
+            this.ramalconsulta.Name = "ramalconsulta";
+            this.ramalconsulta.Size = new System.Drawing.Size(149, 23);
+            this.ramalconsulta.TabIndex = 1;
+            // 
+            // labelchamadas
+            // 
+            this.labelchamadas.AutoSize = true;
+            this.labelchamadas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelchamadas.Location = new System.Drawing.Point(235, 66);
+            this.labelchamadas.Name = "labelchamadas";
+            this.labelchamadas.Size = new System.Drawing.Size(79, 17);
+            this.labelchamadas.TabIndex = 3;
+            this.labelchamadas.Text = "Chamadas:";
+            // 
+            // tipochamada
+            // 
+            this.tipochamada.AutoCompleteCustomSource.AddRange(new string[] {
+            "Realizadas",
+            "Atendidas",
+            "Perdidas"});
+            this.tipochamada.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipochamada.FormattingEnabled = true;
+            this.tipochamada.Items.AddRange(new object[] {
+            "Atendidas",
+            "Perdidas",
+            "Realizadas"});
+            this.tipochamada.Location = new System.Drawing.Point(320, 63);
+            this.tipochamada.Name = "tipochamada";
+            this.tipochamada.Size = new System.Drawing.Size(206, 24);
+            this.tipochamada.TabIndex = 2;
+            this.tipochamada.Text = "Selecione...";
+            // 
+            // dataInicial
+            // 
+            this.dataInicial.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataInicial.Location = new System.Drawing.Point(568, 63);
+            this.dataInicial.Mask = "00/00/0000 90:00:00";
+            this.dataInicial.Name = "dataInicial";
+            this.dataInicial.Size = new System.Drawing.Size(171, 23);
+            this.dataInicial.TabIndex = 3;
+            this.dataInicial.Enter += new System.EventHandler(this.dataInicial_Enter);
+            this.dataInicial.Leave += new System.EventHandler(this.dataInicial_Leave);
+            // 
+            // labelde
+            // 
+            this.labelde.AutoSize = true;
+            this.labelde.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelde.Location = new System.Drawing.Point(532, 66);
+            this.labelde.Name = "labelde";
+            this.labelde.Size = new System.Drawing.Size(30, 17);
+            this.labelde.TabIndex = 5;
+            this.labelde.Text = "De:";
+            // 
+            // dataFinal
+            // 
+            this.dataFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataFinal.Location = new System.Drawing.Point(784, 63);
+            this.dataFinal.Mask = "00/00/0000 90:00:00";
+            this.dataFinal.Name = "dataFinal";
+            this.dataFinal.Size = new System.Drawing.Size(182, 23);
+            this.dataFinal.TabIndex = 4;
+            this.dataFinal.Enter += new System.EventHandler(this.dataFinal_Enter);
+            this.dataFinal.Leave += new System.EventHandler(this.dataFinal_Leave);
+            // 
+            // labelate
+            // 
+            this.labelate.AutoSize = true;
+            this.labelate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelate.Location = new System.Drawing.Point(745, 66);
+            this.labelate.Name = "labelate";
+            this.labelate.Size = new System.Drawing.Size(33, 17);
+            this.labelate.TabIndex = 7;
+            this.labelate.Text = "Até:";
+            // 
+            // calendariode
+            // 
+            this.calendariode.Location = new System.Drawing.Point(568, 92);
+            this.calendariode.Name = "calendariode";
+            this.calendariode.ShowTodayCircle = false;
+            this.calendariode.TabIndex = 0;
+            this.calendariode.Visible = false;
+            this.calendariode.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.calendariode_DateChanged);
+            // 
+            // calendarioate
+            // 
+            this.calendarioate.Location = new System.Drawing.Point(784, 92);
+            this.calendarioate.MaxSelectionCount = 1;
+            this.calendarioate.MinDate = new System.DateTime(2016, 1, 1, 0, 0, 0, 0);
+            this.calendarioate.Name = "calendarioate";
+            this.calendarioate.ShowTodayCircle = false;
+            this.calendarioate.TabIndex = 0;
+            this.calendarioate.Visible = false;
+            this.calendarioate.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.calendarioate_DateChanged);
+            // 
+            // relatoriobuscar
+            // 
+            this.relatoriobuscar.Image = global::AssistenteLigacoes.Properties.Resources.accept;
+            this.relatoriobuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.relatoriobuscar.Location = new System.Drawing.Point(972, 63);
+            this.relatoriobuscar.Name = "relatoriobuscar";
+            this.relatoriobuscar.Size = new System.Drawing.Size(65, 23);
+            this.relatoriobuscar.TabIndex = 11;
+            this.relatoriobuscar.Text = "Buscar";
+            this.relatoriobuscar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.relatoriobuscar.UseVisualStyleBackColor = true;
+            this.relatoriobuscar.Click += new System.EventHandler(this.relatoriobuscar_Click);
+            // 
+            // salvarelatorio
+            // 
+            this.salvarelatorio.Image = global::AssistenteLigacoes.Properties.Resources.page_excel;
+            this.salvarelatorio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.salvarelatorio.Location = new System.Drawing.Point(20, 514);
+            this.salvarelatorio.Name = "salvarelatorio";
+            this.salvarelatorio.Size = new System.Drawing.Size(65, 30);
+            this.salvarelatorio.TabIndex = 12;
+            this.salvarelatorio.Text = "Salvar";
+            this.salvarelatorio.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.salvarelatorio.UseVisualStyleBackColor = true;
+            this.salvarelatorio.Click += new System.EventHandler(this.salvarelatorio_Click);
+            // 
+            // imprimerelatorio
+            // 
+            this.imprimerelatorio.Image = global::AssistenteLigacoes.Properties.Resources.printer;
+            this.imprimerelatorio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.imprimerelatorio.Location = new System.Drawing.Point(969, 514);
+            this.imprimerelatorio.Name = "imprimerelatorio";
+            this.imprimerelatorio.Size = new System.Drawing.Size(65, 30);
+            this.imprimerelatorio.TabIndex = 13;
+            this.imprimerelatorio.Text = "Imprimir";
+            this.imprimerelatorio.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.imprimerelatorio.UseVisualStyleBackColor = true;
+            this.imprimerelatorio.Click += new System.EventHandler(this.imprimerelatorio_Click);
+            // 
+            // printrelatorio
+            // 
+            this.printrelatorio.DocumentName = "Relatorio";
+            this.printrelatorio.BeginPrint += new System.Drawing.Printing.PrintEventHandler(this.printrelatorio_BeginPrint);
+            // 
+            // previewrelatorio
+            // 
+            this.previewrelatorio.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.previewrelatorio.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.previewrelatorio.ClientSize = new System.Drawing.Size(400, 300);
+            this.previewrelatorio.Enabled = true;
+            this.previewrelatorio.Icon = ((System.Drawing.Icon)(resources.GetObject("previewrelatorio.Icon")));
+            this.previewrelatorio.Name = "previewrelatorio";
+            this.previewrelatorio.ShowIcon = false;
+            this.previewrelatorio.Visible = false;
+            // 
+            // RelatorioChamadas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 567);
+            this.Controls.Add(this.imprimerelatorio);
+            this.Controls.Add(this.salvarelatorio);
+            this.Controls.Add(this.relatoriobuscar);
+            this.Controls.Add(this.calendarioate);
+            this.Controls.Add(this.calendariode);
+            this.Controls.Add(this.dataFinal);
+            this.Controls.Add(this.labelate);
+            this.Controls.Add(this.dataInicial);
+            this.Controls.Add(this.labelde);
+            this.Controls.Add(this.tipochamada);
+            this.Controls.Add(this.labelchamadas);
+            this.Controls.Add(this.ramalconsulta);
+            this.Controls.Add(this.labelramal);
+            this.Controls.Add(this.conteudorelatorio);
+            this.MinimumSize = new System.Drawing.Size(1060, 550);
+            this.Name = "RelatorioChamadas";
+            this.Text = "Relatorio de Chamadas";
+            this.Load += new System.EventHandler(this.RelatorioChamadas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.conteudorelatorio)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView conteudorelatorio;
+        private System.Windows.Forms.Label labelramal;
+        private System.Windows.Forms.MaskedTextBox ramalconsulta;
+        private System.Windows.Forms.Label labelchamadas;
+        private System.Windows.Forms.ComboBox tipochamada;
+        private System.Windows.Forms.MaskedTextBox dataInicial;
+        private System.Windows.Forms.Label labelde;
+        private System.Windows.Forms.MaskedTextBox dataFinal;
+        private System.Windows.Forms.Label labelate;
+        private System.Windows.Forms.MonthCalendar calendariode;
+        private System.Windows.Forms.MonthCalendar calendarioate;
+        private System.Windows.Forms.Button relatoriobuscar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn origem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn destino;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tempo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn operadora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cidadeorigem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cidadedestino;
+        private System.Windows.Forms.Button salvarelatorio;
+        private System.Windows.Forms.Button imprimerelatorio;
+        private System.Drawing.Printing.PrintDocument printrelatorio;
+        private System.Windows.Forms.PrintPreviewDialog previewrelatorio;
+    }
+}
---
+++ AssistenteLigacoes/RelatorioChamadas.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+// MetroFramework
+using MetroFramework.Forms;
+
+// Utiliza Interop do Excel
+using Microsoft.Office.Interop.Excel;
+
+namespace AssistenteLigacoes
+{
+    public partial class RelatorioChamadas : MetroForm
+    {
+        public RelatorioChamadas()
+        {
+            // Inicializa o RelatorioChamadas
+            InitializeComponent();
+        }
+
+        private bool verificaData(string data, bool input = false)
+        {
+
+            // Pega apenas os números da data
+            string conteudo = Regex.Match(data, @"\d+").Value;
+
+            // Verifica se é uma data válida
+            DateTime converte;
+            if (!DateTime.TryParse(data, out converte) && (conteudo != "" || input) )
+            {
+
+                // Se foi enviado por um TextBox
+                if (!input)
+                    MessageBox.Show("A data informada não é válida!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return false;
+            }
+
+            return true;
+
+        }
+
+        private void RelatorioChamadas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataInicial_Enter(object sender, EventArgs e)
+        {
+
+            // Torna o calendário visivel
+            calendariode.Visible = true;
+
+        }
+
+        private void dataInicial_Leave(object sender, EventArgs e)
+        {
+
+            // Torna o calendário invisivel
+            calendariode.Visible = false;
+               
+            // Verifica se a data inicial não é válida e limpa o campo
+            if (!verificaData(dataInicial.Text))
+                dataInicial.Text = "";
+
+        }
+
+        private void dataFinal_Enter(object sender, EventArgs e)
+        {
+
+            // Torna o calendário visivel
+            calendarioate.Visible = true;
+
+        }
+
+        private void dataFinal_Leave(object sender, EventArgs e)
+        {
+
+            // Torna o calendário invisivel
+            calendarioate.Visible = false;
+
+            // Verifica se a data final não é válida e limpa o campo
+            if (!verificaData(dataFinal.Text))
+                dataFinal.Text = "";
+
+        }
+
+        private void calendarioate_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+            // Coloca a data selecionada no calendario no TextBox
+            dataFinal.Text = calendarioate.SelectionRange.End.ToString();
+
+        }
+
+        private void calendariode_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+            // Coloca a data selecionada no calendario no TextBox
+            dataInicial.Text = calendariode.SelectionRange.End.ToString();
+
+        }
+
+        private void relatoriobuscar_Click(object sender, EventArgs e)
+        {
+
+            // Verifica se as datas não são válidas
+            if (!verificaData(dataInicial.Text, true) || !verificaData(dataFinal.Text, true))
+            {
+                MessageBox.Show("As datas informadas não são válidas!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dataInicial.Text = "";
+                dataFinal.Text = "";
+                return;
+            }
+
+            // Verifica se o ramal está em branco ou incorreto
+            if (ramalconsulta.Text == "" || ramalconsulta.TextLength != 4)
+            {
+                MessageBox.Show("Preencha o ramal corretamente!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Verifica se o tipo de relatório foi selecionado
+            if (tipochamada.SelectedItem == null)
+            {
+                MessageBox.Show("Escolha o tipo de relatório que deseja!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Converte as datas para DateTime
+            DateTime inicial = DateTime.Parse(dataInicial.Text);
+            DateTime final = DateTime.Parse(dataFinal.Text);
+
+            // Compara as datas
+            int compara = DateTime.Compare(inicial, final);
+
+            if (compara > 0)
+            {
+                MessageBox.Show("A data final não pode ser menor que a data inicial!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+        }
+
+        private void imprimerelatorio_Click(object sender, EventArgs e)
+        {
+            previewrelatorio.Document = printrelatorio;
+            previewrelatorio.ShowDialog();
+        }
+
+        private void printrelatorio_BeginPrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            try
+            {
+                // Calculating Total Widths
+                int iTotalWidth = 0;
+                foreach (DataGridViewColumn dgvGridCol in conteudorelatorio.Columns)
+                {
+                    iTotalWidth += dgvGridCol.Width;
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void salvarelatorio_Click(object sender, EventArgs e)
+        {
+
+            Microsoft.Office.Interop.Excel.Application XcelApp = new Microsoft.Office.Interop.Excel.Application();
+
+            if (conteudorelatorio.Rows.Count > 0)
+            {
+                try
+                {
+                    XcelApp.Application.Workbooks.Add(Type.Missing);
+                    for (int i = 1; i < conteudorelatorio.Columns.Count + 1; i++)
+                    {
+                        XcelApp.Cells[1, i] = conteudorelatorio.Columns[i - 1].HeaderText;
+                    }
+                    //
+                    for (int i = 0; i < conteudorelatorio.Rows.Count - 1; i++)
+                    {
+                        for (int j = 0; j < conteudorelatorio.Columns.Count; j++)
+                        {
+                            XcelApp.Cells[i + 2, j + 1] = conteudorelatorio.Rows[i].Cells[j].Value.ToString();
+                        }
+                    }
+                    //
+                    XcelApp.Columns.AutoFit();
+                    //
+                    XcelApp.Visible = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro : " + ex.Message);
+                    XcelApp.Quit();
+                }
+            }
+
+        }
+    }
+}
