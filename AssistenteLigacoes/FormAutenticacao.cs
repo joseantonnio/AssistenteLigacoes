9f05d0d214c6778daa981584dbddaf16bb17ddf2
José Antonio <jasj_brasil@hotmail.com>
2016-10-27 20:49:19 -0200

    Conexão com MySQL e Prefixos da Anatel

--- AssistenteLigacoes/FormAutenticacao.cs
+++ AssistenteLigacoes/FormAutenticacao.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+
+// Referencia MySQL
+using MySql.Data.MySqlClient;
 
 // MetroFramework
 using MetroFramework.Forms;
@@ -15,6 +19,9 @@ namespace AssistenteLigacoes
 {
     public partial class FormAutenticacao : MetroForm
     {
+
+        // Declara variaveis de conexão com o MySQL
+        private MySqlConnection conexao;
 
         // Declara uma variavel para verificar se o usuário é admin ou não
         private bool isAdmin = false;
@@ -37,61 +44,139 @@ namespace AssistenteLigacoes
             isAdmin = false;
         }
 
+        public static string gerarSHA256(string texto)
+        {
+
+            // Utiliza a classe SHA256 do System Security
+            SHA256 sha256 = SHA256Managed.Create();
+
+            // Converte para bytes e gera a hash
+            byte[] bytes = Encoding.UTF8.GetBytes(texto);
+            byte[] hash = sha256.ComputeHash(bytes);
+
+            // Cria uma string utilizando o StringBuilder
+            StringBuilder resultado = new StringBuilder();
+
+            // Realiza um laço que percorre cada byte do hash
+            for (int i = 0; i < hash.Length; i++)
+            {
+                // Adiciona ao resultado o valor do byte
+                resultado.Append(hash[i].ToString("x2"));
+            }
+
+            // Retorna a hash
+            return resultado.ToString();
+
+    }
+
         private void metroButton1_Click(object sender, EventArgs e)
         {
 
-            // Armazena o usuário e senha em variáveis
-            string u = username.Text;
-            string p = password.Text;
-
-            // Verifica se o usuário passa na autenticação
-            if (u == "admin" && p == "admin" && isAdmin == true)
+            // Verifica se campos foram preenchidos
+            if (username.Text == "")
             {
 
-                // Troca a cor, informa que logou como admin e exibe
-                status.ForeColor = System.Drawing.Color.ForestGreen;
-                status.Text = "Entra como admin...";
+                // Altera a cor para vermelho, informa o erro e exibe
+                status.ForeColor = Color.OrangeRed;
+                status.Text = "Digite o seu usuário...";
                 status.Visible = true;
+
+                // Finaliza a execução do evento
+                return;
+
+            }
+
+            // Verifica se campos foram preenchidos
+            if (password.Text == "")
+            {
+
+                // Altera a cor para vermelho, informa o erro e exibe
+                status.ForeColor = Color.OrangeRed;
+                status.Text = "Digite sua senha...";
+                status.Visible = true;
+
+                // Finaliza a execução do evento
+                return;
+
+            }
+
+            // Armazena o usuário e senha em variáveis
+            string u = username.Text;
+            string p = gerarSHA256(password.Text);
+
+            // Busca usuário e senha no banco de dados
+            MySqlCommand comando = new MySqlCommand("SELECT nome, admin FROM usuarios WHERE usuario = '" + u + "' AND senha = '" + p + "'", conexao);
+
+            // Executa a busca
+            MySqlDataReader busca = comando.ExecuteReader();
+
+            // Verifica se o usuário passa na autenticação
+            if (busca.HasRows)
+            {
+                // Limpa o usuário e senha
+                username.Text = "";
+                password.Text = "";
+
+                // Oculta a mensagem de status
+                status.Visible = false;
+
+                // Oculta o FormAutenticacao
+                Hide();
+
+                // Pega a permissão do usuário
+                busca.Read();
+                bool permissao = busca.GetBoolean(busca.GetOrdinal("admin"));
+
+                // Limpa a busca
+                busca.Close();
+
+                // Verifica se é administrador
+                if (permissao && isAdmin)
+                {
+                    MessageBox.Show("Usuário terá permissões de administrador!");
+                }
+
+                // Inicializa e exibe o FormPrincipal
+                FormPrincipal Principal = new FormPrincipal(this);
+                Principal.Show();
 
             }
             else
             {
-
-                // Verifica se é um usuário comum
-                if (u == "user" && p == "user" && isAdmin == false)
-                {
-
-                    // Limpa o usuário e senha
-                    username.Text = "";
-                    password.Text = "";
-
-                    // Oculta a mensagem de status
-                    status.Visible = false;
-
-                    // Oculta o FormAutenticacao
-                    Hide();
-
-                    // Inicializa e exibe o FormPrincipal
-                    FormPrincipal Principal = new FormPrincipal(this);
-                    Principal.Show();
-
-                }
-                else
-                {
                     
-                    // Altera a cor para vermelho, informa o erro e exibe
-                    status.ForeColor = System.Drawing.Color.OrangeRed;
-                    status.Text = "Usuário e senha incorretos.";
-                    status.Visible = true;
+                // Altera a cor para vermelho, informa o erro e exibe
+                status.ForeColor = Color.OrangeRed;
+                status.Text = "Usuário e senha incorretos.";
+                status.Visible = true;
 
-                }
+                // Limpa a busca
+                busca.Close();
+
+                return;
 
             }
         }
 
         private void FormAutenticacao_Load(object sender, EventArgs e)
         {
-            
+
+            // Define string de conexão
+            conexao = new MySqlConnection("server=localhost;User Id=root;database=assistente_ligacoes; password=rootpass");
+
+            // Abre conexão
+            try
+            {
+                conexao.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Não foi possível conectar ao banco de dados! Entre em contato com o suporte.");
+            }
+        }
+
+        private void FormAutenticacao_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            conexao.Close();
         }
     }
 }
---
+++ assistente_ligacoes.sql
@@ -0,0 +1,58 @@
+CREATE DATABASE  IF NOT EXISTS `assistente_ligacoes` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `assistente_ligacoes`;
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: assistente_ligacoes
+-- ------------------------------------------------------
+-- Server version	5.5.53-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `u_id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(65) NOT NULL,
+  `senha` varchar(128) NOT NULL,
+  `nome` varchar(120) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`u_id`),
+  UNIQUE KEY `usuario_UNIQUE` (`usuario`),
+  UNIQUE KEY `senha_UNIQUE` (`senha`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','Administrador',1),(2,'user','04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb','Usuário',0);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-10-27 20:46:56
